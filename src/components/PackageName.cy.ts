a863df8ad82c9bd51fa0e470a93fc4ee858e2941
mingg123 <spgb123spgb@gmail.com>
2022-10-23 17:18:45 +0900

    feat : add cypress example

---
+++ src/components/PackageName.cy.ts
@@ -0,0 +1,21 @@
+import PackageName from "./PackageName.vue";
+
+describe("<PackageName>", () => {
+  it("mounts", () => {
+    cy.mount(PackageName);
+  });
+
+  const packageNameSelector = "[data-cy=packageName_input]";
+  it("stepper should default to null", () => {
+    cy.mount(PackageName);
+
+    cy.get(packageNameSelector).should("have.text", "");
+  });
+
+  it("packageName should value", () => {
+    cy.mount(PackageName);
+    cy.get(packageNameSelector)
+      .type("게임패키지")
+      .should("have.value", "게임패키지");
+  });
+});
---
+++ src/components/PackageName.vue
@@ -0,0 +1,14 @@
+<template>
+  <input v-model="packageName" data-cy="packageName_input" type="text" />
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "PackageName",
+  setup(props) {
+    const packageName = ref<string>("");
+    return { packageName };
+  },
+});
+</script>
